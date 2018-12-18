81b0a1d0a4b667803668364a24d207104bb4757c
acapone13 <caponeaugusto@gmail.com>
2018-12-18 14:33:06 +0100

    New Upload

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    DB_URI: 'mongodb://Test:test13@ds251747.mlab.com:51747/bwm-ng-dev',
+    SECRET: 'bdjakbfab32awefsf'
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'I am app component from component.ts';
+
+  clickHandler(){
+    alert("I am clicked");
+  }
+}
